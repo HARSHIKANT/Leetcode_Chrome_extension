0a97dea0ca0b6625445a07fb1382ce5d2e7e1a2d
HARSHIKANT <harshikantdubey999@gmail.com>
2025-08-31 12:24:10 +0530

    enhancing the AI

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           const apiKey = "AIzaSyDa7u_aiOZR2GxGt152ZiI0s6RfpgNd5y0";  // <--- replace with your real key
   
           const response = await fetch(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
             {
               method: "POST",
               headers: {
@@ -17,8 +17,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                   {
                     parts: [
                       {
-                        text: `Solve the following coding problem. 
-                        Only give hints, never the full solution/code.
+                        text: `ANSWER ME ONLY UPTO 100 WORDS.\n\n I am providing you my doubt about a CODING question, i have giving the description as well as my code ( may be half written). You have to give me only HINTS and STEPS for solving the problem NEVER give me complete code.
                         
                         Problem: ${request.problem}`
                       }
